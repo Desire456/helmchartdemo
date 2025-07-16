35d16c5deb8ba9aa1538b264c4a35c5dfec9edb1
v.selitskiy <v.selitskiy@tinkoff.ru>
2025-07-16 17:15:07 +0400

    first commit

---
+++ src/main/java/com/desire/helmchartdemo/HelmchartdemoApplication.java
@@ -0,0 +1,13 @@
+package com.desire.helmchartdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelmchartdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelmchartdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desire/helmchartdemo/HelmchartdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desire.helmchartdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelmchartdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
